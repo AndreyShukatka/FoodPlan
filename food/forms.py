7faaf5b572f78972b6bf7dcb187517f03b80005a
AndreyShukatka <106096891+AndreyShukatka@users.noreply.github.com>
2023-03-18 02:21:45 +0300

    Переименовал кнопки

--- food/forms.py
+++ food/forms.py
@@ -6,7 +6,7 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm, SetPassw
 class RegisterUserForm(UserCreationForm):
     username = forms.CharField(label='Логин', widget=forms.TextInput(attrs={'class': 'form-control'}))
     first_name = forms.CharField(label='Имя', widget=forms.TextInput(attrs={'class': 'form-control'}))
-    email = forms.EmailField(required=True, label='Email', widget=forms.EmailInput(attrs={'class':'form-control'}))
+    email = forms.EmailField(required=True, label='Email', widget=forms.EmailInput(attrs={'class': 'form-control'}))
     password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
     password2 = forms.CharField(label='Повтор пароля', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
     fields = [
@@ -19,11 +19,15 @@ class RegisterUserForm(UserCreationForm):
 
 
 class UserProfileForm(UserChangeForm):
-    first_name = forms.CharField(widget=forms.TextInput (attrs={'class': 'form-control', 'readonly': False}))
-    last_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'readonly': False}), required=False)
+    first_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'readonly': False}))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'readonly': False}),
+                                required=False)
     email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control', 'readonly': False}))
-    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'readonly': True}), required=False)
-    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'readonly': True}), required=False)
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'readonly': True}),
+                                required=False)
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'readonly': True}),
+                                required=False)
+
     class Meta:
         model = User
         fields = (
